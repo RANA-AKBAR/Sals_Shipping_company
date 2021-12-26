9a2dbe9024ff54acffb97f00eefa053581757cad
R <RANA.AKBAR.08@gMAIL.COM>
2021-12-25 16:49:03 -0800

    Final shipping method added

--- Shipping_calculator.py
+++ Shipping_calculator.py
@@ -1,13 +1,18 @@
-# Ground Shipping
-# Weight of Package 	Price per Pound 	Flat Charge
-# 2 lb or less 	$1.50 	$20.00
-# Over 2 lb but less than or equal to 6 lb 	$3.00 	$20.00
-# Over 6 lb but less than or equal to 10 lb 	$4.00 	$20.00
-# Over 10 lb 	$4.75 	$20.00
 
-weight = 8.4
+weight = 1.5
 cost_ground = None
-cost_ground_preimum = 125.00 #Fixed Flat fee.
+cost_ground_premium = 125.00 
+cost_drone = None
+
+
+
+
+# Ground Shipping
+# Weight of Package     Price per Pound     Flat Charge
+# 2 lb or less  $1.50   $20.00
+# Over 2 lb but less than or equal to 6 lb  $3.00   $20.00
+# Over 6 lb but less than or equal to 10 lb     $4.00   $20.00
+# Over 10 lb    $4.75   $20.00
 
 # Ground Shipping
 if weight <= 2:
@@ -19,10 +24,7 @@ elif weight > 6 and weight <= 10:
 else:
     cost_ground = weight * 4.75 + 20.00
 
-print("The Total cost of your Shipping is: ", cost_ground)
-
-
-
+print("The Total cost of your Standard Ground Shipping is: ", cost_ground)
 
 
 
@@ -37,3 +39,29 @@ print("The Total cost of your Shipping is: ", cost_ground)
 # Over 2 lb but less than or equal to 6 lb 	$9.00 	$0.00
 # Over 6 lb but less than or equal to 10 lb 	$12.00 	$0.00
 # Over 10 lb
+
+#Fixed Flat fee.
+
+print("The Total cost of your Premium Ground Shipping is: ", cost_ground_premium)
+
+
+# Drone Shipping (new), which has no flat charge, but the rate based on weight is triple the rate of ground shipping.
+# Drone Shipping
+# Weight of Package   Price per Pound     Flat Charge
+# 2 lb or less    $4.50   $0.00
+# Over 2 lb but less than or equal to 6 lb    $9.00   $0.00
+# Over 6 lb but less than or equal to 10 lb   $12.00  $0.00
+# Over 10 lb  $14.25  $0.00
+
+
+if weight <= 2: 
+    cost_drone = weight * 4.50
+elif weight >2 and weight <= 6:
+    cost_drone = weight * 9.00
+elif weight >6 and weight <= 10:
+    cost_drone = weight * 12.00
+else:
+    cost_drone = weight * 14.25
+
+
+print("The Total cost of your Drone Shipping is: ", cost_drone)
