825b86205749114be5f545cc394f9b69bc16c950
R <RANA.AKBAR.08@gMAIL.COM>
2021-12-25 16:29:32 -0800

    Calculator for Basic ( Ground_Shipping ) complete

--- Shipping_calculator.py
+++ Shipping_calculator.py
@@ -1,3 +1,35 @@
-weight = 2
+# Ground Shipping
+# Weight of Package 	Price per Pound 	Flat Charge
+# 2 lb or less 	$1.50 	$20.00
+# Over 2 lb but less than or equal to 6 lb 	$3.00 	$20.00
+# Over 6 lb but less than or equal to 10 lb 	$4.00 	$20.00
+# Over 10 lb 	$4.75 	$20.00
+
+weight = 8.4
+cost_ground = ""
 
 # Ground Shipping
+if weight <= 2:
+    cost_ground = weight * 1.50 + 20.00
+elif weight > 2 and weight <= 6:
+    cost_ground = weight * 3.00 + 20.00
+elif weight > 6 and weight <= 10:
+    cost_ground = weight * 4.00 + 20.00
+else:
+    cost_ground = weight * 4.75 + 20.00
+
+print("The Total cost of your Shipping is: ", cost_ground)
+
+
+
+
+# Ground Shipping Premium
+#
+# Flat charge: $125.00
+#
+# Drone Shipping
+# Weight of Package 	Price per Pound 	Flat Charge
+# 2 lb or less 	$4.50 	$0.00
+# Over 2 lb but less than or equal to 6 lb 	$9.00 	$0.00
+# Over 6 lb but less than or equal to 10 lb 	$12.00 	$0.00
+# Over 10 lb
